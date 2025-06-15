54b0f85eeada0eb830b650c6294a9b7b4330b847
Bùi Mạnh <22022602@vnu.edu.vn>
2025-06-15 23:31:36 +0700

    Xoá agent không dùng đến

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -2,14 +2,10 @@ from .host.host import HostAgent
 from .pm.pm import PMAgent
 from .dev.developer import DeveloperAgent
 from .qa.qa import QAAgent
-from .docu.docu import DocumentationAgent
-from .report.report import ReportAgent
 
 __all__ = [
     'HostAgent',
     'PMAgent',
     'DeveloperAgent',
     'QAAgent',
-    'DocumentationAgent',
-    'ReportAgent'
 ]
